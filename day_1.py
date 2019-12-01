1c5d83b45b8518236d7d54d67f0814d5913304ec
Maria Shornikova <mshornikova@nvidia.com>
2019-12-01 23:45:06 +0300

    Part II

--- day_1.py
+++ day_1.py
@@ -20,8 +20,44 @@ def calculate_required_fuel(module_mass):
     return int(module_mass / 3.0) - 2
 
 
-result = 0
-with open(Path('.') / 'data' / 'day_1_input.txt') as f:
-    for line in f.readlines():
-        result += calculate_required_fuel(int(line))
-print(result)
+def calculate_fuel_for_fuel(fuel):
+    """
+    Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2.
+    However, that fuel also requires fuel, and that fuel requires fuel, and so on.
+    Any mass that would require negative fuel should instead be treated as if it requires zero fuel;
+    the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside
+    the scope of this calculation.
+
+    So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you
+    just calculated as the input mass and repeat the process,
+    continuing until a fuel requirement is zero or negative.
+
+    For example:
+
+    A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0,
+    which would call for a negative fuel), so the total fuel required is still just 2.
+
+    At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2).
+    216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel,
+    which requires no further fuel. So, the total fuel required for a module of mass 1969 is
+    654 + 216 + 70 + 21 + 5 = 966.
+
+    The fuel required by a module of mass 100756 and its fuel is:
+    33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+    """
+    required_fuel = calculate_required_fuel(fuel)
+    return required_fuel + calculate_fuel_for_fuel(required_fuel) if required_fuel > 0 else 0
+
+
+if __name__ == '__main__':
+    required_fuel_total = 0
+    fuel_for_fuel_total = 0
+
+    with open(Path('.') / 'data' / 'day_1_input.txt') as f:
+        for line in f.readlines():
+            required_fuel = calculate_required_fuel(module_mass=int(line))
+            required_fuel_total += required_fuel
+            fuel_for_fuel_total += calculate_fuel_for_fuel(required_fuel)
+
+    print(required_fuel_total)
+    print(required_fuel_total + fuel_for_fuel_total)
--- tests/test_day_1.py
+++ tests/test_day_1.py
@@ -9,12 +9,16 @@ class Day1TestCase(unittest.TestCase):
 
     def test_2(self):
         assert day_1.calculate_required_fuel(14) == 2
+        assert day_1.calculate_fuel_for_fuel(2) == 0
 
     def test_3(self):
         assert day_1.calculate_required_fuel(1969) == 654
+        assert day_1.calculate_fuel_for_fuel(654) == 966 - 654
 
     def test_4(self):
         assert day_1.calculate_required_fuel(100756) == 33583
+        assert day_1.calculate_fuel_for_fuel(33583) == 50346 - 33583
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
